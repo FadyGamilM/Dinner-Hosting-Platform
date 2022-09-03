85a8bb7bb0e0c479d0a1f5639c115825dd4a562a
fady <gamilfady605@gmail.com>
2022-09-03 02:27:11 +0200

    working with Error Handling FIlters

--- DinnerHostingPlatform.API/Program.cs
+++ DinnerHostingPlatform.API/Program.cs
@@ -1,5 +1,8 @@
 using DinnerHostingPlatform.Application;
 using DinnerHostingPlatform.Infrastructure;
+using DinnerHostingPlatform.API.Middleware;
+using DinnerHostingPlatform.API.Filters;
+
 var builder = WebApplication.CreateBuilder(args);
 
 {
@@ -8,12 +11,17 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddApplicationLayerDependencies()
                            .AddInfrastructureLayerDependencies(builder.Configuration);
     // Add services to the container.
-    builder.Services.AddControllers();
+    builder.Services.AddControllers(
+        // use the errorHanldingFilter to handle exceptions on all controllers of this layer
+        options => options.Filters.Add(new ErrorHandlingFilterAttribute())
+    );
 }
 
 var app = builder.Build();
 
 {
+    //* if you need to use global error handling
+    // app.UseMiddleware<ErrorHandlingMiddleware>();
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
